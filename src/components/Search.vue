a4238a73b3a9d27c46cea42e6f643d8fbc78f4ca
Bitcoin Please <support@bchplease.org>
2022-04-14 01:02:17 -0400

    Update API server endpoint.

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -34,7 +34,7 @@
 /* Import modules. */
 import superagent from 'superagent'
 
-const API_ENDPOINT = `https://api.panmoni.com/v1`
+const API_ENDPOINT = `https://api.discover.cash/v1`
 // const API_ENDPOINT = `http://localhost:8080/v1`
 
 export default {
