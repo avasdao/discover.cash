6b5084a06ae54f1e4e65aeab5dd220bb6289e275
Bitcoin Please <support@bchplease.org>
2022-04-14 00:58:33 -0400

    Update sponsors.

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -2,17 +2,17 @@
     <main class="bg-gray-200 flex justify-between px-2 border-t-2 border-b-2 border-gray-400">
         <div class="text-xs text-gray-500 text-center sm:text-left w-full sm:w-1/2">
             Copyright &copy; {{curYear}}
-            <a class="text-blue-500 hover:underline" href="https://panmoni.com" target="_blank">Panmoni</a>.
+            <a class="text-blue-500 hover:underline" href="https://bchplease.org" target="_blank">Bitcoin Please</a>.
             All rights reserved.
         </div>
 
         <div class="hidden sm:block text-xs text-gray-500">
-            <a class="text-blue-500 hover:underline" href="https://github.com/Panmoni/discover-cash" target="_blank">
+            <a class="text-blue-500 hover:underline" href="https://gitlab.com/bchplease/discover-cash" target="_blank">
                 Source
             </a>
             maintained by
-            <a class="text-blue-500 hover:underline" href="https://modenero.com" target="_blank">
-                Modenero
+            <a class="text-blue-500 hover:underline" href="https://bchplease.org" target="_blank">
+                Bitcoin Please
             </a>
         </div>
     </main>
